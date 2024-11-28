38984db4ea4a043dc84bcec4a1879833b562fd63
h-graca <henrique.oliveira.graca@gmail.com>
2024-11-28 12:55:48 +0100

    Update ReciverNew.ino

--- Rover/Servomotors/ReciverNew.ino
+++ Rover/Servomotors/ReciverNew.ino
@@ -1,27 +1,57 @@
 #include <esp_now.h>
 #include <WiFi.h>
+#include <ESP32Servo.h>
 
 // Structure example to receive data
 // Must match the sender structure
 typedef struct struct_message {
-  String dir;
+  String dirX;
+  String dirY;
+  
 } struct_message;
 
 // Create a struct_message called myData
 struct_message myData;
 
+Servo servoX;
+Servo servoY;
+
+int posX=87;
+int posY=63;
+
 // callback function that will be executed when data is received
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   memcpy(&myData, incomingData, sizeof(myData));
-  Serial.print("Direction: ");
-  Serial.println(myData.dir);
+  Serial.print("DirX: ");
+  Serial.println(myData.dirX);
+  Serial.print("DirY: ");
+  Serial.println(myData.dirY);
 
+  if (myData.dirX=="right") {
+    servoX.write(42);
+  }
+  if (myData.dirX=="left") {
+    servoX.write(142);
+  }
+  if (myData.dirY=="down") {
+    servoY.write (30);
+  }
+  if (myData.dirY=="up") {
+    servoY.write(115);
+  }
+  if ((myData.dirX=="center") && (myData.dirY=="center")) {
+    servoX.write(87);
+    servoY.write(63);
+  }  
 }
- 
+
 void setup() {
   // Initialize Serial Monitor
   Serial.begin(115200);
-  
+  servoX.attach(13);
+  servoY.attach(12);
+  servoX.write(posX);
+  servoY.write(posY);
   // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
 
@@ -35,7 +65,6 @@ void setup() {
   // get recv packer info
   esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
 }
- 
 void loop() {
 
 }
